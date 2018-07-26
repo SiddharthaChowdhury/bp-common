2462286e29e6be0feb7d97c034103bf5ab0b8d9d
Siddhartha Chowdhury <siddharthac6@gmail.com>
2018-07-26 15:46:34 +0530

    React achieved

---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>(
+    <div>
+        Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!
+    </div>
+)
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
---
+++ app/webpack.config.js
@@ -0,0 +1,20 @@
+var path    = require('path');
+var hwp     = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, '/src/index.js'),
+    output: {
+        filename: 'build.js',
+        path: path.join(__dirname, '/dist')
+    },
+    module:{
+        rules:[{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins:[
+        new hwp({template:path.join(__dirname, '/src/index.html')})
+    ]
+}
