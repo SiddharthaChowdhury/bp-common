d4073cc6554d9b17bf99f242b314a6f226972b3f
Siddhartha Chowdhury <siddharthac6@gmail.com>
2018-07-30 14:40:43 +0530

    Create account page is created

--- app-react-redux-signup-login-fp/client/components/landing/landing.js
+++ app-react-redux-signup-login-fp/client/components/landing/landing.js
@@ -16,7 +16,36 @@ class Landing extends Component {
     render(){
         return(
             <div className="container">
-                {this.props.regAck.msg}
+                <div className="row justify-content-center align-items-center css-form-cont">
+                    <div className="col-md-6">
+                        <h4 className="text-center">Create account</h4>
+                        <br/>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label><span/>
+                            <input type="text" className="form-control" name="name" id="name"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label><span data-name="email"/>
+                            <input type="email" className="form-control" name="email" id="email"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label><span/>
+                            <input type="password" className="form-control" name="password" id="password"/>
+                        </div>
+                        <div className="text-center">
+                            <div className="css-bottom-label">
+                                <b>Note:</b> By creating account you agree to our <a href="#">tems and conditions</a> &nbsp; | &nbsp; <a href="#">Login?</a>
+                            </div>
+                            <button className="btn btn-primary">Create account</button>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <footer>
+                    <div className="css-copyright">
+                        BySomeone &copy; 2018. &nbsp; | &nbsp; <a href="#">Contact us for query</a>
+                    </div>
+                </footer>
             </div>
         )
     }
